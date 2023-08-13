cc69dc96f8110507b78cb553fa45bceadb271ab9
Jiro <duyanhaa1@gmail.com>
2023-08-13 13:04:55 +0700

    remove login api

--- src/main/java/mm/com/InternetMandalay/controller/LoginController.java
+++ src/main/java/mm/com/InternetMandalay/controller/LoginController.java
@@ -1,31 +1,31 @@
-package mm.com.InternetMandalay.controller;
-
-import mm.com.InternetMandalay.request.LoginRequest;
-import mm.com.InternetMandalay.request.TokenRequest;
-import mm.com.InternetMandalay.service.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@CrossOrigin
-@RequestMapping("/admin")
-public class LoginController {
-    @Autowired
-    private LoginService loginService;
-
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
-        return loginService.login(loginRequest);
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<?> logout(@RequestBody TokenRequest request){
-        return loginService.logout(request);
-    }
-
-    @PostMapping("/refresh-token")
-    public ResponseEntity<?> refreshToken(@RequestBody TokenRequest request){
-        return loginService.refreshToken(request);
-    }
-}
+//package mm.com.InternetMandalay.controller;
+//
+//import mm.com.InternetMandalay.request.LoginRequest;
+//import mm.com.InternetMandalay.request.TokenRequest;
+//import mm.com.InternetMandalay.service.LoginService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@CrossOrigin
+//@RequestMapping("/admin")
+//public class LoginController {
+//    @Autowired
+//    private LoginService loginService;
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
+//        return loginService.login(loginRequest);
+//    }
+//
+//    @PostMapping("/logout")
+//    public ResponseEntity<?> logout(@RequestBody TokenRequest request){
+//        return loginService.logout(request);
+//    }
+//
+//    @PostMapping("/refresh-token")
+//    public ResponseEntity<?> refreshToken(@RequestBody TokenRequest request){
+//        return loginService.refreshToken(request);
+//    }
+//}
